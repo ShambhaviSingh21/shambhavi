ce4e559bfdfc4c7257c88f1cff2e756cedf39bac
ShambhaviSingh21 <75373885+ShambhaviSingh21@users.noreply.github.com>
2021-02-21 17:22:04 +0530

    Add files via upload

---
+++ Stone.js
@@ -0,0 +1,20 @@
+// class Stone{
+    
+//     var options={
+//         isStatic:false,
+//         restitution:0,
+//         friction:1,
+//         density:1.2
+//     }
+//     this.image = loadImage("sprites/stone.png");
+// }
+
+// display(){
+//     var angle = this.body.angle;
+//     push();
+//     translate(this.body.position.x, this.body.position.y);
+//     rotate(angle);
+//     imageMode(CENTER);
+//     image(this.image, 0, 0, this.width, this.height);
+//     pop();
+//   }
